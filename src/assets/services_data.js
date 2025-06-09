670905cfd10cca051573328b1c3351f60effff2e
anujtheboss <anujth345@gmail.com>
2025-06-09 23:26:36 +0545

    width reduced

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,32 +2,32 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Frontend development",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Ability to develop attractive and visually eye catching user interfaces with ease for further integration with backend"
     },
     {
         s_no:"02",
         s_name:"Backend Development",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Ability to code backend logic with frameworks like django,flask and fastapi"
     },
     {
         s_no:"03",
         s_name:"Website Deployment",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Ability to develop complete fullstack website with complete industries standard functionalities."
     },
     {
         s_no:"04",
         s_name:"Data visuallization",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Ability to perform data analysis and visuallization with matplotlib,seaborn and NLP"
     },
     {
         s_no:"05",
         s_name:"ML model Building",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Ability to analyze data,draw insights and build ml models using appropriate algorithms."
     },
     {
         s_no:"06",
-        s_name:"Data cleaning",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Git,Github & Docker",
+        s_desc:"Ability to work with version control tools like git and github and containerize applications with docker."
     },
 ]
  
